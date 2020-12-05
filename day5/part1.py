2b1096d22eab679176db9cf806f5e520fa7a4647
Albert Lázaro <lazaroalbert92@gmail.com>
2020-12-05 17:47:26 +0100

    part1 corrected name function

--- day5/part1.py
+++ day5/part1.py
@@ -1,13 +1,13 @@
 import math
 
-def find(data, min_val, max_val, char_range):
+def binary_find(data, min_val, max_val, char_range):
     if len(data) != 0:
         half = math.ceil((max_val + min_val) / 2.0)
 
         if data[0] == char_range[0]:
-            return find(data[1:], min_val, half, char_range)
+            return binary_find(data[1:], min_val, half, char_range)
         elif data[0] == char_range[1]:
-            return find(data[1:], half, max_val, char_range)
+            return binary_find(data[1:], half, max_val, char_range)
     return min_val
 
 def read_file():
@@ -20,8 +20,8 @@ text = read_file()
 
 max_result = 0
 for boarding_pass in text.split('\n'):
-    row = find(boarding_pass[:7], 0, 127, 'FB')
-    col = find(boarding_pass[7:], 0, 7, 'LR')
+    row = binary_find(boarding_pass[:7], 0, 127, 'FB')
+    col = binary_find(boarding_pass[7:], 0, 7, 'LR')
 
     result = row * 8 + col
     
