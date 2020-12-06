3695bf67e8cacf9bb830bf0841754db9366e4ecb
Albert Lázaro <lazaroalbert92@gmail.com>
2020-12-06 19:35:51 +0100

    day6/part2

---
+++ day6/part2.py
@@ -0,0 +1,44 @@
+def read_file():
+    rd = open('input.txt', 'r')
+    text = rd.read()
+    rd.close()
+    return text
+
+def get_yes_answers_by_group(text):
+    group_count = []
+    answers = {}
+    persons = 0
+    
+    for line in text.split('\n')[:-1]:
+        if len(line) == 0:
+            count = 0
+            for val in answers.values():
+                if val == persons:
+                    count += 1
+            group_count.append(count)
+            persons = 0
+            answers = {}
+
+        else:
+            for char in line:
+                if answers.get(char) == None:
+                    answers[char] = 1
+                else:
+                    answers[char] += 1
+            persons += 1
+
+    count = 0
+    for val in answers.values():
+        if val == persons:
+            count += 1
+    group_count.append(count)
+
+    return group_count
+
+
+
+
+text = read_file()
+results = get_yes_answers_by_group(text)
+
+print sum(results)
