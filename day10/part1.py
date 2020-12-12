2829b698358041f36087792bc35e5c1c4d0086d4
Albert Lázaro <lazaroalbert92@gmail.com>
2020-12-12 13:26:22 +0100

    improved day10/part1

--- day10/part1.py
+++ day10/part1.py
@@ -1,23 +1,16 @@
 def read_file():
     numbers = [0]
-    max_num = 0
     with  open('input.txt', 'r') as reader:
         for line in reader.read().split('\n')[:-1]:
-            number = int(line)
-            numbers.append(number)
-
-            if (number > max_num):
-                max_num = number
-    numbers.append(max_num + 3)
+            numbers.append(int(line))
+    numbers.sort()
+    numbers.append(numbers[-1] + 3)
     return numbers
 
 def process_jolts(jolts):
     diff1 = 0
     diff3 = 0
-    for x in range(len(jolts)):
-        if x + 1 == len(jolts):
-            break
-
+    for x in range(len(jolts)-1):
         if jolts[x+1] - jolts[x] == 3:
                 diff3 += 1
         elif jolts[x+1] - jolts[x] == 2:
@@ -29,6 +22,5 @@ def process_jolts(jolts):
     return diff1 * diff3
 
 jolts = read_file()
-jolts.sort()
 result = process_jolts(jolts)
 print(result)
