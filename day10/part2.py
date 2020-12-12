153aba9dcf725b169e2e481b84aec6b5ddda004f
Albert Lázaro <lazaroalbert92@gmail.com>
2020-12-12 14:12:03 +0100

    day10/part2

--- day10/part2.py
+++ day10/part2.py
@@ -1,29 +1,29 @@
+# Solution obtained in Reddit
+
 def read_file():
     numbers = [0]
-    max_num = 0
-    with  open('input2.txt', 'r') as reader:
+    with  open('input.txt', 'r') as reader:
         for line in reader.read().split('\n')[:-1]:
-            number = int(line)
-            numbers.append(number)
-
-            if (number > max_num):
-                max_num = number
+            numbers.append(int(line))
     numbers.sort()
     numbers.append(numbers[-1] + 3)
     return numbers
 
-def count_combinations(jolts, y=0):
+def count_combinations(jolts):
     count = 1
+    ones = 0
     for x in range(len(jolts) - 1):
-        if jolts[x+1] - jolts[x] == 3:
-            continue
-        elif(jolts[x+1] - jolts[x] == 1):
-            count += count_combinations(jolts[:x+1] + jolts[x+3:], x)
-        elif (jolts[x+1] - jolts[x] == 2):
-            count += count_combinations(jolts[:x+1] + jolts[x+2:], x)
+        if jolts[x+1] - jolts[x] == 1:
+            ones += 1
         else:
-            return 0
-    print(jolts, y)
+            if ones > 1:
+                if ones == 2:
+                    count *= 2
+                elif ones == 3:
+                    count *= 4
+                elif ones == 4:
+                    count *= 7
+            ones = 0
     return count
 
 jolts = read_file()
